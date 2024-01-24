7f23546e853694c28b996f1d1a71ae6e231c3d86
MaggieShpileva <shpileva.rita@gmail.com>
2024-01-25 02:23:34 +0300

    added api

--- next.config.js
+++ next.config.js
@@ -1,61 +1,115 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  mode: "development",
-  experimental: {
-    appDir: true,
-  },
+// /**
+// * @type {import('next').NextConfig}
 
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/home?lang=ru",
-        permanent: false,
-      },
-      {
-        source: "/ru",
-        destination: "/home",
-        locale: false,
-        permanent: false,
-      },
-    ];
-  },
-  reactStrictMode: false,
+// */
+// const nextConfig = {
+//   mode: "development",
+//   experimental: {
+//     appDir: true,
+//   },
 
-  images: {
-    unoptimized: true,
-    domains: [
-      "avatars.mds.yandex.net",
-      "www.kinopoisk.ru",
-      "st.kp.yandex.net",
-      "kinopoiskapiunofficial.tech",
-      "widgets.kinopoisk.ru",
-      "www.themoviedb.org",
-      "img02.rl0.ru",
-      "ic.pics.livejournal.com",
-      "seeklogo.com",
-      "static.vl.ru",
-      "thumbs.dfs.ivi.ru",
-      "i0.wp.com",
-      "static.okko.tv",
-      "images.kinorium.com",
-    ],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "www.kinopoisk.ru",
-        port: "",
-        pathname: "/account123/**",
-      },
-    ],
-  },
-  contentSecurityPolicy: {
-    directives: {
-      defaultSrc: ["'self'"],
-      frameSrc: ["https://www.youtube.com"],
-      // ...
+//   async redirects() {
+//     return [
+//       {
+//         source: "/",
+//         destination: "/home?lang=ru",
+//         permanent: false,
+//       },
+//       {
+//         source: "/ru",
+//         destination: "/home",
+//         locale: false,
+//         permanent: false,
+//       },
+//     ];
+//   },
+//   reactStrictMode: false,
+
+//   images: {
+//     unoptimized: true,
+//     domains: [
+//       "avatars.mds.yandex.net",
+//       "www.kinopoisk.ru",
+//       "st.kp.yandex.net",
+//       "kinopoiskapiunofficial.tech",
+//       "widgets.kinopoisk.ru",
+//       "www.themoviedb.org",
+//       "img02.rl0.ru",
+//       "ic.pics.livejournal.com",
+//       "seeklogo.com",
+//       "static.vl.ru",
+//       "thumbs.dfs.ivi.ru",
+//       "i0.wp.com",
+//       "static.okko.tv",
+//       "images.kinorium.com",
+//     ],
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "www.kinopoisk.ru",
+//         port: "",
+//         pathname: "/account123/**",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
+module.exports = async (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    mode: "development",
+    experimental: {
+      appDir: true,
     },
-  },
-};
 
-module.exports = nextConfig;
+    async redirects() {
+      return [
+        {
+          source: "/",
+          destination: "/home?lang=ru",
+          permanent: false,
+        },
+        {
+          source: "/ru",
+          destination: "/home",
+          locale: false,
+          permanent: false,
+        },
+      ];
+    },
+    reactStrictMode: false,
+
+    images: {
+      unoptimized: true,
+      domains: [
+        "avatars.mds.yandex.net",
+        "www.kinopoisk.ru",
+        "st.kp.yandex.net",
+        "kinopoiskapiunofficial.tech",
+        "widgets.kinopoisk.ru",
+        "www.themoviedb.org",
+        "img02.rl0.ru",
+        "ic.pics.livejournal.com",
+        "seeklogo.com",
+        "static.vl.ru",
+        "thumbs.dfs.ivi.ru",
+        "i0.wp.com",
+        "static.okko.tv",
+        "images.kinorium.com",
+      ],
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "www.kinopoisk.ru",
+          port: "",
+          pathname: "/account123/**",
+        },
+      ],
+    },
+  };
+  return nextConfig;
+};
